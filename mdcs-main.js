b6ae955f77622b7613b5b87ae9384d332198eea9
Joshua Koo <zz85nus@gmail.com>
2016-11-16 00:20:18 +0800

    Support additional ESLint presets

--- mdcs-main.js
+++ mdcs-main.js
@@ -1,2 +1,17 @@
 window.CodeMirror = require('./node_modules/codemirror/lib/codemirror');
 // window.JscsStringChecker = require('./node_modules/jscs/lib/string-checker.js');
+
+
+window.eslint_configs = {
+    // TODO have a proper ESLINT config loader!!!
+
+    standard: require('eslint-config-standard'),
+    // airbnb: require('eslint-config-airbnb-base'),
+    google: require('eslint-config-google'),
+    grunt: require('eslint-config-grunt'),
+    // idiomatic: require('eslint-config-idiomatic'),
+    // jquery: require('eslint-config-jquery'),
+    // node: require('eslint-config-node-style-guide'),
+    wikimedia: require('eslint-config-wikimedia'),
+    wordpress: require('eslint-config-wordpress'),
+}
